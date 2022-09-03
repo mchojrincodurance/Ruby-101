b58e947638616863a5449b9ce98acd6352a13080
Mauro Chojrin <mauro.chojrin@codurance.com>
2022-09-03 16:23:22 +0200

    Failing test

--- test/fizz_buzz_test.rb
+++ test/fizz_buzz_test.rb
@@ -8,3 +8,5 @@ assert_kind_of FizzBuzz, fizz_buzz, "fizz_buzz should be an instance of FizzBuzz
 assert_equal "1", fizz_buzz.convert(1)
 assert_equal "2", fizz_buzz.convert(2)
 assert_equal "Fizz", fizz_buzz.convert(3)
+assert_equal "Buzz", fizz_buzz.convert(5)
+
