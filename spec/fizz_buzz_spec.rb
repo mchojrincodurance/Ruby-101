0d376e061541222f62699bd75d8847b3bb325f44
Mauro Chojrin <mauro.chojrin@codurance.com>
2022-09-05 11:14:34 +0200

    Using parameterized tests

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -3,9 +3,9 @@ require 'fizz_buzz'
 describe FizzBuzz do
 
   describe ".convert" do
-    [3, 6].each do |num|
-      it "should return 'fizz' for multiples of three (#{num})" do  
-        expect(FizzBuzz.convert(num)).to eq("Fizz")
+    [[3, "Fizz"], [6, "Fizz"]].each do |num, str|
+      it "should return '#{str}' when converting #{num}" do  
+        expect(FizzBuzz.convert(num)).to eq(str)
       end
     end
   end
