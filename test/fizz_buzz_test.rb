1fa80b86a9e8f952ab87945199e569f13ce80cab
Mauro Chojrin <mauro.chojrin@codurance.com>
2022-09-05 10:16:54 +0200

    Running tests

--- test/fizz_buzz_test.rb
+++ test/fizz_buzz_test.rb
@@ -1,14 +1,17 @@
-require 'fizz_buzz.rb'
-require "test/unit/assertions"
-include Test::Unit::Assertions
+require "test/unit"
+require_relative '../lib/fizz_buzz.rb'
 
-fizz_buzz = FizzBuzz.new
-assert_kind_of FizzBuzz, fizz_buzz, "fizz_buzz should be an instance of FizzBuzz"
+class FizzBuzzTest < Test::Unit::TestCase
 
-assert_equal "1", fizz_buzz.convert(1)
-assert_equal "2", fizz_buzz.convert(2)
-assert_equal "Fizz", fizz_buzz.convert(3)
-assert_equal "Buzz", fizz_buzz.convert(5)
-assert_equal "Fizz", fizz_buzz.convert(6)
-assert_equal "Buzz", fizz_buzz.convert(10)
-assert_equal "FizzBuzz", fizz_buzz.convert(15)
+  def setup
+    @fizz_buzz = FizzBuzz.new
+  end
+
+  def test_should_create_the_right_object
+    assert_kind_of FizzBuzz, @fizz_buzz, "fizz_buzz should be an instance of FizzBuzz"
+  end
+
+  def test_should_convert_non_special_numbers
+    assert_equal "1", @fizz_buzz.convert(1)
+  end
+end
