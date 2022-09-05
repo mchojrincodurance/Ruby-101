faee09155e766fce701ecc597ee7925c2b5f83c9
Mauro Chojrin <mauro.chojrin@codurance.com>
2022-09-05 11:15:45 +0200

    Failing test

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -3,7 +3,14 @@ require 'fizz_buzz'
 describe FizzBuzz do
 
   describe ".convert" do
-    [[3, "Fizz"], [6, "Fizz"]].each do |num, str|
+    [
+      [3, "Fizz"], 
+      [6, "Fizz"], 
+      [1, "1"], 
+      [2, "2"],
+      [15, "FizzBuzz"],
+      [20, "20"] 
+    ].each do |num, str|
       it "should return '#{str}' when converting #{num}" do  
         expect(FizzBuzz.convert(num)).to eq(str)
       end
